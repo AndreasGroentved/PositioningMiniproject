0bceddd148053d7b5f58be7c5191000c27057427
Andreas Grøntved <andreasgro@gmail.com>
2018-03-12 16:29:16 +0100

    Gradle update

--- app/src/main/java/com/group/unkown/positioningminiproject/ui/MainActivity.java
+++ app/src/main/java/com/group/unkown/positioningminiproject/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.group.unkown.positioningminiproject;
+package com.group.unkown.positioningminiproject.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.group.unkown.positioningminiproject.R;
 
 public class MainActivity extends AppCompatActivity {
 
